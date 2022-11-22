7989ec7eeecf52e583ecd2ec8d7ac761b8d519e7
Ayush Bhagwan Fanse <87874012+AyushFanse@users.noreply.github.com>
2022-11-22 17:40:27 +0530

    task-techo

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React from "react";
+import service_data from "../../Data/Blogs.json";
 
 const Services = () => {
     return (
         <section>
-            <div className='services_nav_outer'>
-                <nav className='services_nav'>
-                    <ul className='services_nav_inner flex jc ac row'>
-                        <li className='services_nav_tab'>BLOGS</li>
-                        <li className='services_nav_tab'>VIDEOS</li>
-                        <li className='services_nav_tab'>BOOKS</li>
+            <div className="services_nav_outer">
+                <nav className="services_nav">
+                    <ul className="services_nav_inner flex jc ac row">
+                        <li>
+                            <button className="services_nav_tab active">
+                                BLOGS
+                            </button>
+                        </li>
+                        <li>
+                            <button className="services_nav_tab">VIDEOS</button>
+                        </li>
+                        <li>
+                            <button className="services_nav_tab">BOOKS</button>
+                        </li>
                     </ul>
                 </nav>
             </div>
-            <div></div>
+            <div className="card_container flex jc ac">
+                {service_data.map((item, i) => (
+                    <div className="card_outer">
+                        <div key={item.title + i}>
+                            <div className='card'>
+                                <div className="card_img">
+                                    <img src={item.thumbnail} alt="" />
+                                </div>
+                                <h1>{item.title}</h1>
+                                <p>{item.content}</p>
+                                <button className="read_btn">Read more</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </section>
     );
 };
